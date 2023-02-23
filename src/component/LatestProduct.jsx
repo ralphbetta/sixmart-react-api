9ab7c85462c51b6bda56bccd0a3a9c01d1087c4a
ralphbetta <ralphbetta@gmail.com>
2023-02-24 00:18:11 +0100

    implemented sorting

--- src/component/LatestProduct.jsx
+++ src/component/LatestProduct.jsx
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from "react";
+import Skeleton from "react-loading-skeleton";
 
 
 // import { useNavigate } from 'react-router-dom'
@@ -8,7 +9,7 @@ const image = "https://picsum.photos/200/300"
 const LatestProduct = () => {
 
     const [data, setData] = useState([]);
-    const [filter, setFilter] = useState(data);
+    const [filteredList, setFilteredList] = useState(data);
     const [loading, setLoading] = useState(false);
     let componentMounted =  true;
 
@@ -26,7 +27,7 @@ const LatestProduct = () => {
 
             if(componentMounted){
                 setData(await response.clone().json());
-                setFilter(await response.json())
+                setFilteredList(await response.json())
                 setLoading(false)
             }
             return ()=>{
@@ -39,12 +40,22 @@ const LatestProduct = () => {
         getProducts();
     }, [])
 
-    console.log(filter);
+    console.log(filteredList);
+
+        
+    const filterProduct = (param) => {
+         setFilteredList(data)
+        console.log({param})
+        const updatedList = filteredList.filter((x)=> x.category === param)
+        console.log(updatedList)
+        setFilteredList(updatedList)
+    }
+
 
   return (
     <div className='mt-8'>
 
-      {loading ? <ProductGridLoader/> : <ProductGrid filter={filter}/> }
+      {loading ? <ProductGridLoader/> : <ProductGrid filteredList={filteredList} data={data} handleFilter={filterProduct} setFilteredList={setFilteredList} /> }
      
     </div>
   )
@@ -57,44 +68,58 @@ export default LatestProduct
 
 const ProductGridLoader = () => {
     return (
-        <div>
-            Loading....
+      <div>
+        <div className="justify-center h-52  col-span-3">
+           <Skeleton className=""/>
         </div>
+        <div className="justify-center h-52  col-span-3">
+           <Skeleton className=""/>
+        </div>
+        <div className="justify-center h-52  col-span-3">
+           <Skeleton className=""/>
+        </div>
+        <div className="justify-center  h-52 col-span-3">
+           <Skeleton className=""/>
+        </div>
+      </div>
      );
 }
+
+
+ 
  
 
 
 
 // Refactored Grid Component
 
-const ProductGrid = ({ filter }) => {
+const ProductGrid = ({ filteredList, data, handleFilter, setFilteredList }) => {
+
+
+
     return ( 
         <div>
-        <u className='flex list-none no-underline gap-3 justify-center mb-10'>
-            <li className='border-[1.5px] border-gray-500 px-2 rounded-sm text-gray-500 bg-white hover:bg-gray-500 hover:text-white cursor-pointer'>All</li>
-            <li className='border-[1.5px] border-gray-500 px-2 rounded-sm text-gray-500 bg-white hover:bg-gray-500 hover:text-white cursor-pointer'>Men's Clothing</li>
-            <li className='border-[1.5px] border-gray-500 px-2 rounded-sm text-gray-500 bg-white hover:bg-gray-500 hover:text-white cursor-pointer'>Women's Clothing</li>
-            <li className='border-[1.5px] border-gray-500 px-2 rounded-sm text-gray-500 bg-white hover:bg-gray-500 hover:text-white cursor-pointer'>Jewelery</li>
-            <li className='border-[1.5px] border-gray-500 px-2 rounded-sm text-gray-500 bg-white hover:bg-gray-500 hover:text-white cursor-pointer'>Eclectronic</li>
-        </u>
+        <div className='flex list-none no-underline gap-3 justify-center mb-10'>
+            <button onClick={()=>setFilteredList(data)} className='border-[1.5px] border-gray-500 px-2 rounded-sm text-gray-500 bg-white hover:bg-gray-500 hover:text-white cursor-pointer'>All</button>
+            <button onClick={()=>handleFilter("men's clothing")} className='border-[1.5px] border-gray-500 px-2 rounded-sm text-gray-500 bg-white hover:bg-gray-500 hover:text-white cursor-pointer'>Men's Clothing</button>
+            <button onClick={()=>handleFilter("women's clothing")} className='border-[1.5px] border-gray-500 px-2 rounded-sm text-gray-500 bg-white hover:bg-gray-500 hover:text-white cursor-pointer'>Women's Clothing</button>
+            <button onClick={()=>handleFilter("jewelery")} className='border-[1.5px] border-gray-500 px-2 rounded-sm text-gray-500 bg-white hover:bg-gray-500 hover:text-white cursor-pointer'>Jewelery</button>
+            <button onClick={()=>handleFilter("Electronic")} className='border-[1.5px] border-gray-500 px-2 rounded-sm text-gray-500 bg-white hover:bg-gray-500 hover:text-white cursor-pointer'>Electronic</button>
+        </div>
 
         <div className=" w-full grid md:grid-cols-4 sm:grid-cols-2 gap-5 px-[5%]">
  
        {
        
-       filter.map((product)=>{
+       filteredList.map((product, index)=>{
         
         return (
-            <div className=" shadow-md p-2 rounded-md border-2 border-gray-100 items-center justify-center flex flex-col">
-                <div>
-                    {/* {product} */}
-                </div>
+            <div className=" shadow-md p-2 rounded-md border-2 border-gray-100 items-center justify-center flex flex-col" key={product.id}>
                 <div className=" overflow-hidden sm:h-70 w-full rounded-lg flex justify-center items-center">
                     <img src={product.image} alt="" className=' h-64' />
                 </div>
-                <h4 className='py-2'>Random Title</h4>
-                <h4 className=' font-semibold mb-2'>$190.99</h4>
+                <h4 className='py-2 ali text-center text-gray-600 text-[15px]'>{product.title}...</h4>
+                <h4 className=' font-semibold mb-2'>${product.price}</h4>
                 <button className='border-[1.5px] border-gray-500 px-2 mb-4 rounded-sm text-gray-500 bg-white hover:bg-gray-500 hover:text-white cursor-pointer'>Buy Now</button>
             </div>
         )
